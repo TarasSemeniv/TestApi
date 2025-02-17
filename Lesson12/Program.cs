70ac51ea811a73d2ae9559cb5691ef27e285205e
Тарас Семенів <tarassemeniv2@gmail.com>
2025-02-17 19:26:28 +0100

    add endpoint

---
+++ Lesson12/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/api/sayHello", () => Results.Ok("Say Hello"));
+
+app.Run();
+
